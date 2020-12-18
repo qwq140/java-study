e9b4b44bc893ad926b698de23851d2ac6764242e
qwq140 <qwq140@gmail.com>
2020-12-18 12:02:05 +0900

    java study

---
+++ src/test/fsdfs.java
@@ -0,0 +1,12 @@
+package test;
+
+import java.time.LocalTime;
+
+public class fsdfs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(LocalTime.now());
+	}
+
+}
